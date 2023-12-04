f1d3436ba19751140f4232541bf9112539087aea
shyogaswara <91866594+shyogaswara@users.noreply.github.com>
2023-12-05 05:30:28 +0700

    Add files via upload
    added method for saving the output into xyz

--- comcot2xyz.py
+++ comcot2xyz.py
@@ -3,12 +3,32 @@ This code is used to convert comcot output from each layer into XYZ file either
 for zmax (inundation), or ETA. the output will be layer{LN}_x.dat for longitude,
 layer{LN}_y.dat for latitude, ttt_layer{LN}.dat for ETA, and zmax_layer{LN}.dat
 for zmax where LN is Layer Number.
+
+To use this code, first, user need to specify the path of layers that generated
+by Comcot and also the layer number, just like line 266-267 and line 270. User 
+can also use the code to process many layer at once by spcefied the start number
+and end number of those layers while also making sure the layers file exist
+in same path.
+
+The code written by shyogaswara, for further question please contact me at
+sh.yogaswara@gmail.com
+
+--------------
+PYTHON_VERSION
+--------------
+3.x
+
+---------
+LIBRARIES
+---------
+math
+os
+re
 '''
 
 __version__ = '0.12.2023'
 __author__ = 'shyogaswara'
 
-import numpy as np
 import math
 import os
 import re
@@ -21,8 +41,9 @@ class Comcot2XYZ:
     ----------
     Attributes
     ----------
-    layer_num : str
-        a layer number string from comcot output based on their ID, ex: 03
+    layer_num : str, int
+        a layer number from comcot output based on their ID, ex: 03
+        can be string or integer, but can only have numerical character
     fpath : str
         string for file path of the layer   
           
@@ -64,7 +85,7 @@ class Comcot2XYZ:
             row number from a file to be considered as one line in list
             that needed to be equal with the length of X coordinates for ttt
             file
-        line_number2 : int
+        line_number2 : list
             list that consists of line_number_zmax and line_number_ttt for
             iterating purpose
         dt_reshape : list
@@ -82,6 +103,7 @@ class Comcot2XYZ:
             dt.close()
         
         self.x_length = len(self.dt[0])
+        self.y_length = len(self.dt[1])
         if len(self.dt[2][0].split()) == len(self.dt[3][0].split()):
             line_number = math.ceil(self.x_length/len(self.dt[2][0].split()))
         else:
@@ -94,15 +116,18 @@ class Comcot2XYZ:
             ln=0
             try:
                 dt_reshape.append(self.reshape_data(line_number, i))
-            except:
+            except: 
                 dt_reshape.append(
                     self.reshape_data(line_number2[ln], i)
                     )
                 ln += 1                
+        #print(len(dt_reshape[0]))
         
         self.dt_xyz=[]
         for i in dt_reshape:
             self.dt_xyz.append(self.generate_xyz(i))
+        
+        self.save_file(fpath,layer_num)
     
     def get_file(self, layer_num):
         '''Get file name string from layer_num
@@ -112,7 +137,7 @@ class Comcot2XYZ:
         ----------
         layer_num : str, int
             Layer number from comcot output ID to be processed
-            can be string or integer but cannot contain alphabet characters
+            can be string or integer, but can only have numerical character
         
         ------
         Raises
@@ -150,8 +175,7 @@ class Comcot2XYZ:
         Attributes
         ----------
         fpath : str
-            Path of file to be opened
-        
+            Path of file to be opened 
         fname : str
             filename that will be opened
         '''
@@ -187,7 +211,7 @@ class Comcot2XYZ:
             leftover list from data_file started from line_number onward
         '''
         matrix_data = []
-        for x in range(self.x_length):
+        for y in range(self.y_length):
             dt_o = data_file[:line_number]
             res = " ".join(dt_o).split()
             matrix_data.append(res)
@@ -196,34 +220,80 @@ class Comcot2XYZ:
     
     def generate_xyz(self,data_file):
         '''Generate 3d array data from x, y, and reshaped data of zmax and 
-        travel time from reshape_data method
+        travel time from reshape_data method. Matrx_data will be cointan value
+        like x[0],y[0],z[0][0] ; x[0],y[1],z[1][0] etc
         
         ---------
         Atributes
         ---------
         data_file : list
             list of value from data zmax or travel time that already reshaped
+        
+        ----------
+        Parameters
+        ----------
+        matrix_data : list
+            list of data in 3D array format consisted of x, y, and z
+        i, j : int
+            indexes from list
+        x, y : str
+            each row string from list
         '''
         matrix_data = []
         for i, x in enumerate(self.dt[0]):
-            for j, y in enumerate(reversed(self.dt[1])):
+            for j, y in enumerate(self.dt[1]):
                 matrix_data.append(
                     [item.strip() for item in [x,y,data_file[j][i]]]
                     )
         return matrix_data
     
     def save_file(self, fpath, layer_num):
-        pass
+        '''Save the file in corresponding path and layer number.
+        the zmax output is stored in meters while travel time in seconds
+        
+        ----------
+        Attributes
+        ----------
+        fpath : str
+            Path of file to be writened
+        layer_num : str, int
+            layer number string from comcot output based on their ID, ex: 03
+            can be string or integer, but can only have numerical character
+        
+        ----------    
+        Parameters
+        ----------
+        i : int
+            indexes from list
+        f : str
+            value from list separately
+        to_write : str
+            value from xyz list to be written  
+        '''
+        for i, f in enumerate(['zmax','ttt']):
+            fopen = open(os.path.join(fpath, f'{f}_{layer_num}.xyz'),'w')
+            for item in self.dt_xyz[i]:
+                to_write = f'{item}'
+                to_write = to_write[1:-1].replace("'",'')
+                fopen.write(f'{to_write}\n')
+            fopen.close()
         
 
 
 
 if __name__=="__main__":
     MAIN_PATH = r"D:\Tsunami_Model_SUMBAR"
-    FILE_PATH = r"SumBar\resSB_2layer"
+    FILE_PATH = r"SumBar\res_1"
     FILE_PATH = os.path.join(MAIN_PATH,FILE_PATH)
 
-    LAYER_NUM = '2'
-
-    dt = Comcot2XYZ(LAYER_NUM,FILE_PATH)
+    LAYER_NUM = 1
     
+    #For single layer
+    #dt = Comcot2XYZ(LAYER_NUM,FILE_PATH)
+    
+    
+    #For many layer at once
+    START_LAYER = 21
+    END_LAYER = 30
+    for LN in range(START_LAYER,END_LAYER+1):
+        dt = Comcot2XYZ(LN,FILE_PATH)
